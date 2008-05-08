0a85799c6718908ff490f64439e37079bf50392f
dan@inlet.geol.sc.edu <dan@inlet.geol.sc.edu@826288e8-f448-0410-b6ac-112692871d22>
2008-05-08 19:55:20 +0000

    Added command line option to pass optional style sheet in.

--- trunk/obskml/QAQC/GenPlatformUptimeWebpage.pl
+++ trunk/obskml/QAQC/GenPlatformUptimeWebpage.pl
@@ -10,6 +10,12 @@ use Getopt::Long;
 #Define to allow the script to be tested on another machine with local data.
 #Set to 0 if running in a Linux/Unix environment. This mostly deals with how file paths are handled, as well as a couple
 #of shell commands.
+##############################################################################################################################
+#Revisions
+#Rev: 1.1.0.0
+#Author: DWR
+#Changes: Added command line option to pass optional style sheet in.
+##############################################################################################################################
 use constant MICROSOFT_PLATFORM => 0;
 
 use constant
@@ -26,17 +32,25 @@ my $PLATFORMHTMLFILE = "PlatformUptimePercentages.html";
 
 my %CommandLineOptions;
 GetOptions( \%CommandLineOptions,
-            "WorkingDir=s" );
+            "WorkingDir=s",
+            "StyleSheet:s"); #DWR v1.1.0.0
 
 my $strWorkingDir   = $CommandLineOptions{"WorkingDir"};   
 if( length( $strWorkingDir ) == 0 )
 {
 
-  die print( "Missing required field(s).\n".
+  die print( "Missing required field or fields.\n".
               "Command Line format: -WorkingDir\n". 
-              "-WorkingDir provides the path to the directory where the $PLATFORMCSVFILE resides for a given provider. HTML file created will be created here as well.\n" );  
+              "--WorkingDir provides the path to the directory where the $PLATFORMCSVFILE resides for a given provider. HTML file created will be created here as well.\n".
+              "--StyleSheet optional argument to give the style sheet to use in formatting the page.\n") ;  
 }
-
+#DWR v1.1.0.0
+my $strStyleSheet = $CommandLineOptions{"StyleSheet"}; 
+# If no style sheet provided, use the default.
+if( length( $strStyleSheet ) == 0 )
+{
+  $strStyleSheet = 'http://carocoops.org/~dramage_prod/styles/main.css';
+}
 my $PlatformPercentCSVFile;
 my $PlatformPercentHTMLFile;
 
@@ -54,7 +68,7 @@ else
 print( $PlatformPercentHTMLFile "<html>\n" );
 print( $PlatformPercentHTMLFile  "<BODY>\n" );
 
-print( $PlatformPercentHTMLFile "<link href=\"http://carocoops.org/~dramage_prod/styles/main.css\" rel=\"stylesheet\" type=\"text/css\" media=\"screen\" />\n" );
+print( $PlatformPercentHTMLFile "<link href=\"$strStyleSheet\" rel=\"stylesheet\" type=\"text/css\" media=\"screen\" />\n" );
 my $iTestProfileNdx = 0;
 #my @ReportIntervals;
 
@@ -72,15 +86,9 @@ foreach my $strLine (<$PlatformPercentCSVFile>)
 	  {
     	print( $PlatformPercentHTMLFile "<tr><td><BR></td></tr>" );
 	    
-	  #  print( $PlatformPercentHTMLFile "</table><BR><BR>" );
 	  }
 	  #Start a new table per new header.
-	  #print( $PlatformPercentHTMLFile "<table border=\"1\">" );
-
-	  #For each new haeder, let's add a row to help show this.
-    #my $strBuf = "<CAPTION><H2>Sensor Reporting Percentage</H2></CAPTION>";
-	  #print( $PlatformPercentHTMLFile $strBuf );
-	  
+  
 	  $iTestProfileNdx++;
 	  $iWriteTableHeader = 1;
 	} 
@@ -105,7 +113,8 @@ foreach my $strLine (<$PlatformPercentCSVFile>)
   	    #my( $strBuf, $iInterval) = split( / /, $strInterval );
   	    #push( @ReportIntervals, $iInterval );
   	  }
-  		print $PlatformPercentHTMLFile "<TH><STRONG>$strColumn</STRONG></TH>\n";	  
+  		#print $PlatformPercentHTMLFile "<TH><STRONG>$strColumn</STRONG></TH>\n";	  
+      print $PlatformPercentHTMLFile "<TH>$strColumn</TH>\n";	  
   	}
  		print $PlatformPercentHTMLFile "</THEAD >\n";	  
   }
@@ -116,20 +125,24 @@ foreach my $strLine (<$PlatformPercentCSVFile>)
   	while (@Columns) 
   	{
   	  my $strColumn = shift(@Columns);
- 	    my $rgbColor = RGB_DEFAULT;  	    
+ 	    #my $rgbColor = RGB_DEFAULT;  	    
+      my $strBGColorID = "RGB_DEFAULT";
   	  if( $iCnt >= 3 )
   	  {
   	    if( $strColumn >= 90.0 )
   	    {
-  	     $rgbColor = RGB_90THPERCENTILE;
+  	     #$rgbColor = RGB_90THPERCENTILE;
+         $strBGColorID = "RGB_90THPERCENTILE";
   	    }
   	    elsif( $strColumn >= 70.0 )
   	    {
-  	     $rgbColor = RGB_70THPERCENTILE;  	      
+  	     #$rgbColor = RGB_70THPERCENTILE;  	      
+         $strBGColorID = "RGB_70THPERCENTILE";
   	    }
   	    else
   	    {
-  	      $rgbColor = RGB_UNDER70TH;
+  	      #$rgbColor = RGB_UNDER70TH;
+          $strBGColorID = "RGB_UNDER70TH";
   	    }
   	  }
   	  # First 3 columns are platform, url, and date.
@@ -143,7 +156,8 @@ foreach my $strLine (<$PlatformPercentCSVFile>)
           $strColumn = "<A HREF=\"$strURL\">$strColumn</A>";       		    	      
   	    }
   	  }	  
-  		print $PlatformPercentHTMLFile "<td bgcolor=\"$rgbColor\" >$strColumn</td>\n";    		
+  		#print $PlatformPercentHTMLFile "<td bgcolor=\"$rgbColor\" >$strColumn</td>\n";    		
+      print $PlatformPercentHTMLFile "<td id=\"$strBGColorID\" >$strColumn</td>\n";    		
   	  $iCnt++;
   	}
     print( $PlatformPercentHTMLFile "</tr>\n" );
