af6f4e9c7e55b122dced0769c099469b61c51915
dan@inlet.geol.sc.edu <dan@inlet.geol.sc.edu@826288e8-f448-0410-b6ac-112692871d22>
2013-12-17 13:56:19 +0000

    Date: 2013-12-17 Function: processData Changes: Added the ability to specify specific csvReader object.

--- trunk/postgresql/feeds/fwri/fwriHabmonIngestion.py
+++ trunk/postgresql/feeds/fwri/fwriHabmonIngestion.py
@@ -1,3 +1,10 @@
+"""
+Revisions
+Date: 2013-12-17
+Function: processData
+Changes: Added the ability to specify specific csvReader object.
+"""
+
 import logging.config
 
 import optparse
@@ -7,13 +14,59 @@ import urllib2
 import socket
 import Queue
 import ConfigParser
+from datetime import datetime
 
-from xeniatools.xeniaSQLAlchemy import  multi_obs 
-from xeniatools.xenia import uomconversionFunctions
 from xeniatools.DataIngestion import xeniaDataIngestion, dataSaveWorker
-from xeniatools.csvDataIngestion import csvDataIngestion
+from xeniatools.csvDataIngestion import csvDataIngestion, xeniaCSVReader
 
 
+class fwriCSVReader(xeniaCSVReader):
+  #def __init__(self, fileObj, xeniaMappingFile, uomConverter, dialect='excel', logger=False, *args, **kwds):
+  #  xeniaCSVReader.__init__(self, fileObj, xeniaMappingFile, uomConverter, dialect, logger, args, kwds)
+
+  def getPlatformData(self, row):
+    dataRec = {}
+    #Get the platform column. If we don't have one, we use the default value.
+    platformCol  = self.xeniaMapping.getPlatformColumn()
+    if(platformCol == None):
+      platformHandle = self.xeniaMapping.getPlatformDefaultHandle();
+    else:
+      #THis will be the short name, we then need to look up the platform_handle
+      platformHandle = row['platformCol']
+
+    dataRec['platform_handle'] = platformHandle
+
+    #Get the date entry as we need it for each observation.
+    dateCol, dateFormat, timezone = self.xeniaMapping.getDateColumn()
+    #Get the data once for use in each observation.
+    dateVal = row[dateCol]
+    if(dateVal.isalnum() == False):
+      dateVal = "".join(i for i in dateVal if ord(i)<128)
+    try:
+      dataRec['m_date'] = datetime.strptime(dateVal, dateFormat)
+    except Exception,e:
+      if(self.logger):
+        self.logger(e)
+
+    #Check to see if there is GPS in the data, if so we'll use it, otherwise we'll use the default.
+    dataRec['m_lat'] = dataRec['m_lon'] = None
+    if('GpsLatDeg' in row):
+      #lat/lon are in degree and minutes column, combine the 2.
+      try:
+        dataRec['m_lat'] = float(row['GpsLatDeg']) + float(row['GpsLatMin']) / 60.0
+        dataRec['m_lon'] = float(row['GpsLonDeg']) + float(row['GpsLonMin']) / 60.0
+      except (ValueError,Exception) as e:
+        if(self.logger):
+          self.logger.error("Lat or Lon value is not a float: LatDeg: %s LatMin: %s LonDeg: %s LonMin: %s" %\
+                            (row['GpsLatDeg'], row['GpsLatMin'], row['GpsLonDeg'], row['GpsLonMin']))
+    else:
+      #Get the longitude and latitude columns. If they don't exist, use the default values.
+      lonCol,latCol = self.xeniaMapping.getFixedLonLatColumn()
+      if(lonCol == None and latCol == None):
+        dataRec['m_lon'], dataRec['m_lat'] = self.xeniaMapping.getDefaultFixedLonLat()
+
+
+    return(dataRec)
 
 
 class fwriCSVDataIngestion(xeniaDataIngestion):
@@ -35,12 +88,27 @@ class fwriCSVDataIngestion(xeniaDataIngestion):
     for platformName in self.platformList:
       if(self.logger):
         self.logger.info("Processing platform: %s" % (platformName))
+      #DWR 2013-12-17
+      #Get the specific csvReader object to use.
+      csvReaderObj = None
+      try:
+        csvReaderName = self.config.get(platformName, 'csvReader')
+      except ConfigParser.Error, e:
+        if(self.logger):
+          self.logger.error("Platform: %s Could not find csvReader ini parameter." % (platformName))
+      else:
+        if(csvReaderName in globals()):
+          csvReaderObj = globals()[csvReaderName]
+        else:
+          if(self.logger):
+            self.logger.error("Platform: %s Could not find csvReader: %s" % (platformName, csvReaderName))
+
       #Attempt to download the latest file.
       useLogger = False
       if(self.logger):
         useLogger = True
       processObj = csvDataIngestion(platformName, self.configFilename, useLogger)
-      if(processObj.initialize()):
+      if(processObj.initialize(csvReader = csvReaderObj)):
         csvUrl = self.config.get(platformName, 'csvURL')
         if(self.logger):
           self.logger.debug("Requesting page: %s" % (csvUrl))
