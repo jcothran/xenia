90086d634f24db343ce829d9bd334a2eda50714a
dan <dan@inlet.geol.sc.edu>
2014-04-24 15:06:19 +0000

    FTP mods

--- postgresql/remotesensing/avhrr_sst/get_latest_listing.pl
+++ postgresql/remotesensing/avhrr_sst/get_latest_listing.pl
@@ -1,11 +1,12 @@
 #!/usr/local/bin/perl
 # Last modified:  Time-stamp: <2003-09-25 11:45:33 haines>
 #
-# Abstract:  get listing of files with "latest.nc" in name from http index, 
+# Abstract:  get listing of files with "latest.nc" in name from http index,
 #
 # Usage:  % get_latest_listing.pl [-d]
 #
-# Author: Sara Haines (2003-09) 
+# Author: Sara Haines (2003-09)
+
 
 # Processing:
 #    (1) Screen scrape http index listing sent by server for all HREF data
@@ -19,7 +20,7 @@ if (grep /[debug|DEBUG|d]/, @ARGV) {
 # add libraries needed for this function
 use POSIX qw(strftime);
 
-# 
+#
 $now = strftime("%Y:%m:%d %H:%M:%S",gmtime);
 if ($debug) { print "\n==== Starting: $now UTC ==== Perl Version: $]\n"; }
 
@@ -28,15 +29,15 @@ $dir_url = $this_dir_url;
 
 # $time_stamp = strftime("%Y%m%d",gmtime);
 
-$common = 'n*.fullpass.true.png'; 
+$common = 'n*.fullpass.true.png';
 
 # @latest = get_index_match($dods_url, $common);
 
 # print "------------------------------" . "\n";
 # print "Common file names scraped from " . "\n";
 # print "$dods_url:". "\n";
-# for (@latest) { 
-#    print($_, "\n"); 
+# for (@latest) {
+#    print($_, "\n");
 # }
 
 @latest = get_index_match($dir_url, $common);
@@ -44,8 +45,8 @@ $common = 'n*.fullpass.true.png';
 # print "------------------------------" . "\n";
 # print "Common file names scraped from " . "\n";
 # print "$dir_url:". "\n";
-# for (@latest) { 
-#    print($_, "\n"); 
+# for (@latest) {
+#    print($_, "\n");
 # }
 # ----------------------------------------------------------------
 # subroutines
@@ -60,28 +61,60 @@ sub get_index_match {
 
     # add libraries needed for this function
     use LWP::Simple;
-    
+    use URI;
+
     # passed parameters
     my ($path, $pattern) = @_;
+    my $isFTP = 0;
 
-    # escape any regexp chars given by user 
+    #2014-04-22 DWR
+    #We need to check the URL to see if it is an FTP site or an HTML directory.
+    my $uriObj = URI->new($path);
+    print("Connection type: " . $uriObj->scheme . "\n");
+    if($uriObj->scheme == 'ftp')
+    {
+      $isFTP = 1;
+    }
+    # escape any regexp chars given by user
     # (so $pattern can't be constructed regexp)
     # $pattern =~ s/([\*\.\<\>\{\}\[\]\^\$\|\+\?\\\/])/\\$1/g;
 
     # (0) Get html document
     $doc = get($path);
+    print "$doc\n";
+    #2014-04-22 DWR
+    #We need to check the URL to see if it is an FTP site or an HTML directory.
+    if($isFTP)
+    {
+        my @fileLines = split('\n', $doc);
+        foreach $fileLine (@fileLines)
+        {
+          my @parts = split(" ", $fileLine);
+          my $fileName = $parts[-1];
+          #print("$fileName\n");
+          push(@matched, $fileName);
+        }
+    }
+    else
+    {
+        # (0) Get html document
+        $doc = get($path);
 
-    # some other possible html scrapes
-    # @all_href = $doc =~ m{href=(.*?)>}gi;
-    # @all_href = $doc =~ m{href\s*=\s*(.*?)>}gi;
+        # some other possible html scrapes
+        # @all_href = $doc =~ m{href=(.*?)>}gi;
+        # @all_href = $doc =~ m{href\s*=\s*(.*?)>}gi;
 
-    # (1) Screen scrape http for href lines
-    @all_href = $doc =~ m{href\s*=[\s|"]*(.*?)[\s|"]*>}gi;
+        # (1) Screen scrape http for href lines
+        @all_href = $doc =~ m{href\s*=[\s|"]*(.*?)[\s|"]*>}gi;
 
-    
-    # (2) further limit with users pattern
-    @matched = grep /$pattern$/, @all_href;
 
+        # (2) further limit with users pattern
+        @matched = grep /$pattern$/, @all_href;
+    }
+    foreach(@matched)
+    {
+        print "Matched: _$\n";
+    }
     return @matched;
 
 } # sub glob_html
